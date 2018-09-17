300697cbd123fb2dce5d4f9a80ccc1dd581a477b
Medric Bruel <medric49@gmail.com>
2018-09-17 16:43:35 +0100

    Initialisation du projet

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="container">
+        <google-map></google-map>
+    </div>
+</template>
+
+<script>
+    import GoogleMap from './components/GoogleMap'
+
+
+    export default {
+        name: 'app',
+        data() {
+            return {
+            }
+        },
+        components : {
+            GoogleMap
+        }
+    }
+</script>
+
+<style lang="scss">
+    @font-face {
+        font-family: 'Raleway';
+        src: url("/assets/fonts/Raleway-Light.ttf");
+    }
+
+    body {
+        font-family: 'Raleway', sans-serif;
+        font-size: 1rem;
+    }
+
+</style>
---
+++ src/components/GoogleMap.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="row">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "GoogleMap"
+    }
+</script>
+
+<style scoped>
+
+</style>
